9f5e35e6bbe986ad1dcd71e5b8ca3db6401b15ba
Hunter Quebedeaux <hunterq@knights.ucf.edu>
2021-04-22 21:55:26 -0400

    redid the integration function for fixed time step, performs as well as ivp

--- python numerical solution/main.py
+++ python numerical solution/main.py
@@ -6,31 +6,31 @@ import math
 L = 0.5
 g = 9.81
 
-b = 0.3
+b = 0.1
 m = 1
 
-def pendulumODE(t,theta):
+
+def pendulumODE(theta,t):
     dtheta1 = theta[1]
     dtheta2 = -g/L*math.sin(theta[0])
-    return dtheta1, dtheta2
+    return [dtheta1, dtheta2]
 
-
-def pendulumODEFriction(t, theta):
+def pendulumODEFriction(theta,t):
     dtheta1 = theta[1]
     dtheta2 = -b/m*theta[1]-g/L*math.sin(theta[0])
-    return dtheta1, dtheta2
+    return [dtheta1, dtheta2]
 
-t0,tf = 0,20
-t = np.linspace(t0, tf, 100)
+t0,tf = 0,10
+t = np.arange(t0, tf, 0.01)
 
 theta0 = [(math.pi/180) * 80, (math.pi/180) * 0]
 
-r = integrate.solve_ivp(pendulumODEFriction, (t0, tf), theta0, "LSODA")
+r = integrate.odeint(pendulumODEFriction, theta0, t)
 # LSODA is the closest integrator to ODE45 in matlab
 
-print(r.y.shape)
+# print(r.y.shape)
 # print(r.y)
 
 
-plt.plot(r.t,r.y[0])
+plt.plot(t,r[:,0])
 plt.show()
