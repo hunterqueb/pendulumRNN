e03aea325cd52517ae1c5a3389b0b3e48a888f21
Hunter Quebedeaux <hunter.quebedeaux@ucf.edu>
2025-04-15 14:39:08 -0400

    updated hohmann script using new args

--- TSCscriptHohmann.sh
+++ TSCscriptHohmann.sh
@@ -7,7 +7,7 @@
 force_constants=(10000 100 1)
 
 # Modes to test (third argument to Python script)
-modes=(0 1)
+modes=(4 2)
 
 # Output directory (Git Bash format for C:\ drive)
 output_dir="/c/Users/hu650776/SynologyDrive/TSCHohmannOutputLogs"
@@ -27,6 +27,6 @@ for mode in "${modes[@]}"; do
     echo "Running with force constant: $force_constant, only using pos data: $mode"
 
     # Execute the Python script with the force constant and mode
-    python scripts/classification/mambaTimeSeriesBinaryClassificationHohmann.py "$force_constant" 10000 "$mode" | tee "$output_file"
+    python scripts/classification/mambaTimeSeriesBinaryClassificationHohmann.py --deltaV "$force_constant" --trainDim "$mode" --no-plot --transformer | tee "$output_file"
   done
 done
