1e0e27d47455a24aa5d362d1a0045e781ed21d08
Hunter Quebedeaux <hunter.quebedeaux@ucf.edu>
2025-10-02 17:12:57 -0400

    added new scripts for generating classification results

--- generateJournalThrustClass.sh
+++ generateJournalThrustClass.sh
@@ -1,57 +1,81 @@
 #!/bin/bash
 
 #in distribution VLEO
-## 3 minute cart
-python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
-    --orbit vleo --test vleo --systems 800 --testSys 800 \
-    --norm --noise --propMin 3 --train_ratio 0.2 --save
 ## 10 minute cart
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit vleo --test vleo --systems 800 --testSys 800 \
     --norm --noise --propMin 10 --train_ratio 0.2 --save
-## 3 minute OE
+## 30 minute cart
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit vleo --test vleo --systems 800 --testSys 800 \
-    --norm --noise --propMin 3 --train_ratio 0.2 --OE --save
+    --norm --noise --propMin 30 --train_ratio 0.2 --save
+## 100 minute cart
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit vleo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 100 --train_ratio 0.2 --save
 ## 10 minute OE
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit vleo --test vleo --systems 800 --testSys 800 \
     --norm --noise --propMin 10 --train_ratio 0.2 --OE --save
+## 30 minute OE
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit vleo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 30 --train_ratio 0.2 --OE --save
+## 100 minute OE
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit vleo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 100 --train_ratio 0.2 --OE --save
 
 # out of distribution
 
 # LEO to VLEO
-## 3 minute cart
-python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
-    --orbit leo --test vleo --systems 800 --testSys 800 \
-    --norm --noise --propMin 3 --train_ratio 0.2 --save
 ## 10 minute cart
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit leo --test vleo --systems 800 --testSys 800 \
     --norm --noise --propMin 10 --train_ratio 0.2 --save
-## 3 minute OE
+## 30 minute cart
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit leo --test vleo --systems 800 --testSys 800 \
-    --norm --noise --propMin 3 --train_ratio 0.2 --OE --save
+    --norm --noise --propMin 30 --train_ratio 0.2 --save
+## 100 minute cart
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit leo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 100 --train_ratio 0.2 --save
 ## 10 minute OE
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit leo --test vleo --systems 800 --testSys 800 \
     --norm --noise --propMin 10 --train_ratio 0.2 --OE --save
+## 30 minute OE
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit leo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 30 --train_ratio 0.2 --OE --save
+## 100 minute OE
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit leo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 100 --train_ratio 0.2 --OE --save
 
 # LEO-MEO-GEO to VLEO
-## 3 minute cart
-python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
-    --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
-    --norm --noise --propMin 3 --train_ratio 0.2 --save
 ## 10 minute cart
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
     --norm --noise --propMin 10 --train_ratio 0.2 --save
-## 3 minute OE
+## 30 minute cart
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
-    --norm --noise --propMin 3 --train_ratio 0.2 --OE --save
+    --norm --noise --propMin 30 --train_ratio 0.2 --save
+## 100 minute cart
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 100 --train_ratio 0.2 --save
 ## 10 minute OE
 python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
     --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
     --norm --noise --propMin 10 --train_ratio 0.2 --OE --save
+## 30 minute OE
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 30 --train_ratio 0.2 --OE --save
+## 100 minute OE
+python scripts/classification/mambaTimeSeriesClassificationGMATThrusts.py \
+    --orbit combined/leo-meo-geo --test vleo --systems 800 --testSys 800 \
+    --norm --noise --propMin 100 --train_ratio 0.2 --OE --save
